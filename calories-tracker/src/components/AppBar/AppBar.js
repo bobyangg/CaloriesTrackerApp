854df992b70695da4b3eb982f361fd12b1d9bbf6
Bob Yang <bobyang2004@gmail.com>
2023-11-20 15:14:17 -0500

    Changed name
    Changed title

--- calories-tracker/src/components/AppBar/AppBar.js
+++ calories-tracker/src/components/AppBar/AppBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AppBar = () =>{
   return (
     <div className="app__navbar">
-        <h1>Calories Tracker<span>App</span></h1>
+        <h1>CalTracker<span>+</span></h1>
     </div>
   );
 }
